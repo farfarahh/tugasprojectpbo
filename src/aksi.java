240cc3a138de91039f2d629f2efcbaed72f6c9b9
farfarahh <farahmardho@gmail.com>
2021-06-23 21:01:56 +0800

    tugas project pbo

---
+++ src/aksi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class aksi {
+    public static void main(String[] args) {
+        Scanner inputan = new Scanner(System.in);
+        String nama = inputan.nextLine();
+
+        makeup beauty = new makeup();
+        beauty.brand = "Maybelline";
+    }
+}
---
+++ src/makeup.java
@@ -0,0 +1,13 @@
+public class makeup {
+    //attribute
+    private String brand;
+    private String namaMakeup;
+    private String jenis;
+    private int kode;
+    private int jumlah;
+    private String jenisKulit;
+    private String jenisTekstur;
+    private String harga;
+}
+
+    public void 
